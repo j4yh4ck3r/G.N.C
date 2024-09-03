773693842a2bb7712a53f645e76a755f37211db1
j4yh4ck3r <124062266+j4yh4ck3r@users.noreply.github.com>
2024-09-03 16:19:52 -0300

    index.html

---
+++ js/scripts.js
@@ -0,0 +1,330 @@
+function gerador() {
+    var sorteio1 = Math.floor(Math.random() * 9 + 0);
+    var sorteio2 = Math.floor(Math.random() * 9 + 0);
+    var sorteio3 = Math.floor(Math.random() * 9 + 0);
+    var sorteio4 = Math.floor(Math.random() * 9 + 0);
+    var sorteio5 = Math.floor(Math.random() * 9 + 0);
+    var sorteio6 = Math.floor(Math.random() * 9 + 0);
+    var sorteio7 = Math.floor(Math.random() * 9 + 0);
+    var sorteio8 = Math.floor(Math.random() * 9 + 0);
+    
+    const numeros1 = document.querySelector("#numero1");
+    const numeros2 = document.querySelector("#numero2");
+    const numeros3 = document.querySelector("#numero3");
+    const numeros4 = document.querySelector("#numero4");
+    const numeros5 = document.querySelector("#numero5");
+    const numeros6 = document.querySelector("#numero6");
+    const numeros7 = document.querySelector("#numero7");
+    const numeros8 = document.querySelector("#numero8");
+
+    switch (sorteio1) {
+
+        case 0:
+            numeros1.innerHTML = "0";
+            break;
+        case 1:
+            numeros1.innerHTML = "1";
+            break;
+        case 2:
+            numeros1.innerHTML = "2";
+            break;
+        case 3:
+            numeros1.innerHTML = "3";
+            break;
+        case 4:
+            numeros1.innerHTML = "4";
+            break;
+        case 5:
+            numeros1.innerHTML = "5";
+            break;
+        case 6:
+            numeros1.innerHTML = "6";
+            break;
+        case 7:
+            numeros1.innerHTML = "7";
+            break;
+        case 8:
+            numeros1.innerHTML = "8";
+            break;
+        case 9:
+            numeros1.innerHTML = "9";
+            break;
+        default:
+            break;
+    }
+
+    switch (sorteio2) {
+
+        case 0:
+            numeros2.innerHTML = "0";
+            break;
+        case 1:
+            numeros2.innerHTML = "1";
+            break;
+        case 2:
+            numeros2.innerHTML = "2";
+            break;
+        case 3:
+            numeros2.innerHTML = "3";
+            break;
+        case 4:
+            numeros2.innerHTML = "4";
+            break;
+        case 5:
+            numeros2.innerHTML = "5";
+            break;
+        case 6:
+            numeros2.innerHTML = "6";
+            break;
+        case 7:
+            numeros2.innerHTML = "7";
+            break;
+        case 8:
+            numeros2.innerHTML = "8";
+            break;
+        case 9:
+            numeros2.innerHTML = "9";
+            break;
+        default:
+            break;
+    }
+
+    switch (sorteio3) {
+
+        case 0:
+            numeros3.innerHTML = "0";
+            break;
+        case 1:
+            numeros3.innerHTML = "1";
+            break;
+        case 2:
+            numeros3.innerHTML = "2";
+            break;
+        case 3:
+            numeros3.innerHTML = "3";
+            break;
+        case 4:
+            numeros3.innerHTML = "4";
+            break;
+        case 5:
+            numeros3.innerHTML = "5";
+            break;
+        case 6:
+            numeros3.innerHTML = "6";
+            break;
+        case 7:
+            numeros3.innerHTML = "7";
+            break;
+        case 8:
+            numeros3.innerHTML = "8";
+            break;
+        case 9:
+            numeros3.innerHTML = "9";
+            break;
+        default:
+            break;
+    }
+
+    switch (sorteio4) {
+
+        case 0:
+            numeros4.innerHTML = "0";
+            break;
+        case 1:
+            numeros4.innerHTML = "1";
+            break;
+        case 2:
+            numeros4.innerHTML = "2";
+            break;
+        case 3:
+            numeros4.innerHTML = "3";
+            break;
+        case 4:
+            numeros4.innerHTML = "4";
+            break;
+        case 5:
+            numeros4.innerHTML = "5";
+            break;
+        case 6:
+            numeros4.innerHTML = "6";
+            break;
+        case 7:
+            numeros4.innerHTML = "7";
+            break;
+        case 8:
+            numeros4.innerHTML = "8";
+            break;
+        case 9:
+            numeros4.innerHTML = "9";
+            break;
+        default:
+            break;
+    }
+
+    switch (sorteio5) {
+
+        case 0:
+            numeros5.innerHTML = "0";
+            break;
+        case 1:
+            numeros5.innerHTML = "1";
+            break;
+        case 2:
+            numeros5.innerHTML = "2";
+            break;
+        case 3:
+            numeros5.innerHTML = "3";
+            break;
+        case 4:
+            numeros5.innerHTML = "4";
+            break;
+        case 5:
+            numeros5.innerHTML = "5";
+            break;
+        case 6:
+            numeros5.innerHTML = "6";
+            break;
+        case 7:
+            numeros5.innerHTML = "7";
+            break;
+        case 8:
+            numeros5.innerHTML = "8";
+            break;
+        case 9:
+            numeros5.innerHTML = "9";
+            break;
+        default:
+            break;
+    }
+
+    switch (sorteio6) {
+
+        case 0:
+            numeros6.innerHTML = "0";
+            break;
+        case 1:
+            numeros6.innerHTML = "1";
+            break;
+        case 2:
+            numeros6.innerHTML = "2";
+            break;
+        case 3:
+            numeros6.innerHTML = "3";
+            break;
+        case 4:
+            numeros6.innerHTML = "4";
+            break;
+        case 5:
+            numeros6.innerHTML = "5";
+            break;
+        case 6:
+            numeros6.innerHTML = "6";
+            break;
+        case 7:
+            numeros6.innerHTML = "7";
+            break;
+        case 8:
+            numeros6.innerHTML = "8";
+            break;
+        case 9:
+            numeros6.innerHTML = "9";
+            break;
+        default:
+            break;
+    }
+
+    switch (sorteio7) {
+
+        case 0:
+            numeros7.innerHTML = "0";
+            break;
+        case 1:
+            numeros7.innerHTML = "1";
+            break;
+        case 2:
+            numeros7.innerHTML = "2";
+            break;
+        case 3:
+            numeros7.innerHTML = "3";
+            break;
+        case 4:
+            numeros7.innerHTML = "4";
+            break;
+        case 5:
+            numeros7.innerHTML = "5";
+            break;
+        case 6:
+            numeros7.innerHTML = "6";
+            break;
+        case 7:
+            numeros7.innerHTML = "7";
+            break;
+        case 8:
+            numeros7.innerHTML = "8";
+            break;
+        case 9:
+            numeros7.innerHTML = "9";
+            break;
+        default:
+            break;
+    }
+
+    switch (sorteio8) {
+
+        case 0:
+            numeros8.innerHTML = "0";
+            break;
+        case 1:
+            numeros8.innerHTML = "1";
+            break;
+        case 2:
+            numeros8.innerHTML = "2";
+            break;
+        case 3:
+            numeros8.innerHTML = "3";
+            break;
+        case 4:
+            numeros8.innerHTML = "4";
+            break;
+        case 5:
+            numeros8.innerHTML = "5";
+            break;
+        case 6:
+            numeros8.innerHTML = "6";
+            break;
+        case 7:
+            numeros8.innerHTML = "7";
+            break;
+        case 8:
+            numeros8.innerHTML = "8";
+            break;
+        case 9:
+            numeros8.innerHTML = "9";
+            break;
+        default:
+            break;
+    }
+}
+
+const DDI = document.querySelector("#DDI");
+
+function inputDDI(){
+    const inputDDI = document.querySelector("#input-DDI");
+
+    DDI.innerHTML = inputDDI.value;
+}
+const DDD = document.querySelector("#DDD");
+
+function inputDDD(){
+    const inputDDD = document.querySelector("#input-DDD");
+    
+    DDD.innerHTML = inputDDD.value;
+}
+
+function filtroTeclas (event) {
+    return ((event.charCode >= 48 && event.charCode <= 57));
+}
+
+function reniciar(){
+    location.href = "./indexcallsorte.html";
+}
